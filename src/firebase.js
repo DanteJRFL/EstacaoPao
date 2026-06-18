f87d97ae16716e4761609e1307765d8593451f0d
ssantanapgs <rpggustavo56@gmail.com>
2026-06-18 15:14:42 -0300

    updating

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyALJaQPIVYf1hxSuamiclnVTHFnvp_BJ6A",
@@ -7,8 +8,7 @@ const firebaseConfig = {
   projectId: "estacaopao",
   storageBucket: "estacaopao.firebasestorage.app",
   messagingSenderId: "159121175816",
-  appId: "1:159121175816:web:8b7edc309f042abf9be8bd",
-  measurementId: "G-M4GH8P0PBE"
+  appId: "1:159121175816:web:8b7edc309f042abf9be8bd"
 };
 
 const app = initializeApp(firebaseConfig);
--- src/menu.js
+++ src/menu.js
@@ -1,50 +1,51 @@
 import { db } from "./firebase.js";
 
 import {
-  collection,
-  getDocs
-} from "firebase/firestore";
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const grid = document.getElementById("produtos-grid");
 
 async function carregarProdutos() {
 
-  try {
-
-    const querySnapshot = await getDocs(
-      collection(db, "produtos")
+    const snapshot = await getDocs(
+        collection(db, "produtos")
     );
 
     grid.innerHTML = "";
 
-    querySnapshot.forEach((doc) => {
+    snapshot.forEach((doc) => {
 
-      const produto = doc.data();
+        const produto = doc.data();
 
-      grid.innerHTML += `
-        <div class="card">
-          <div class="card-content">
+        grid.innerHTML += `
+          <div class="card">
 
-            <h3 class="card-title">
-              ${produto.nome}
-            </h3>
+            <img
+              src="${produto.imagem}"
+              alt="${produto.nome}"
+            >
 
-            <p>
-              ${produto.descricao}
-            </p>
+            <div class="card-content">
 
-            <div class="card-price">
-              R$ ${produto.preco.toFixed(2)}
+              <h3 class="card-title">
+                ${produto.nome}
+              </h3>
+
+              <p>
+                ${produto.descricao}
+              </p>
+
+              <div class="card-price">
+                R$ ${Number(produto.preco).toFixed(2)}
+              </div>
+
             </div>
 
           </div>
-        </div>
-      `;
+        `;
     });
-
-  } catch (erro) {
-    console.error(erro);
-  }
 }
 
 carregarProdutos();
